611a576e166c03500328347635a2d4db00e9b489
Gilbert Emerson <emer7@users.noreply.github.com>
2020-09-09 21:23:11 +0800

    Close popup menu after selecting

--- src/PopupMenu.js
+++ src/PopupMenu.js
@@ -6,24 +6,31 @@ export const Popupmenu = ({
   anchorEl,
   handleClosePopupMenu,
   handleOpenCrossReferenceDialog,
-}) => (
-  <Popover
-    open={Boolean(anchorEl)}
-    anchorEl={anchorEl}
-    onClose={handleClosePopupMenu}
-    anchorOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    transformOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
-    }}
-  >
-    <ButtonGroup size="small" color="primary">
-      <Button>Highlight</Button>
-      <Button onClick={handleOpenCrossReferenceDialog}>Cross-reference</Button>
-      <Button>Copy</Button>
-    </ButtonGroup>
-  </Popover>
-);
+}) => {
+  const handleClickCrossReference = () => {
+    handleClosePopupMenu();
+    handleOpenCrossReferenceDialog();
+  };
+  
+  return (
+    <Popover
+      open={Boolean(anchorEl)}
+      anchorEl={anchorEl}
+      onClose={handleClosePopupMenu}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+    >
+      <ButtonGroup size="small" color="primary">
+        <Button>Highlight</Button>
+        <Button onClick={handleClickCrossReference}>Cross-reference</Button>
+        <Button>Copy</Button>
+      </ButtonGroup>
+    </Popover>
+  );
+};
