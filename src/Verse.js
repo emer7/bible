ae443d492ebe00ce046458f396017c30a8d3c514
Gilbert Emerson <emer7@users.noreply.github.com>
2020-09-09 21:23:22 +0800

    Add cursor pointer to verse

--- src/Verse.js
+++ src/Verse.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { styled } from '@material-ui/core/styles';
 
 import esv from './ESV.json';
 
+const SpanWithPointer = styled('span')({
+  cursor: 'pointer',
+});
+
 export const Verse = ({ book, chapter, verse, handleVerseClick }) => {
   return (
-    <span onClick={handleVerseClick}>
+    <SpanWithPointer onClick={handleVerseClick}>
       {verse} {esv[book][chapter][verse]}
-    </span>
+    </SpanWithPointer>
   );
 };
