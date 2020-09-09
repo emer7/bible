ba02600e47cb7004795d5690a53c0f9fadf01cf5
Gilbert Emerson <emer7@users.noreply.github.com>
2020-09-09 21:18:16 +0800

    Link verse selector, popup, and cross reference

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
-function App() {
+import { VerseSelector } from './VerseSelector';
+import { Popupmenu } from './PopupMenu';
+import { CrossReferenceDialog } from './CrossReferenceDialog';
+
+export const App = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+
+  const handleOpenPopupMenu = e => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClosePopupMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenCrossReferenceDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseCrossReferenceDialog = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
-    <div className="App">
-      Hello
+    <div>
+      <VerseSelector handleVerseClick={handleOpenPopupMenu} />
+      <Popupmenu
+        anchorEl={anchorEl}
+        handleClosePopupMenu={handleClosePopupMenu}
+        handleOpenCrossReferenceDialog={handleOpenCrossReferenceDialog}
+      />
+      <CrossReferenceDialog
+        open={isDialogOpen}
+        handleCloseCrossReferenceDialog={handleCloseCrossReferenceDialog}
+      />
     </div>
   );
-}
+};
 
 export default App;
